282498ef1fe8fb11cea0289e79f5d06cf3a63ae0
Jaden White <jaw65980@email.vccs.edu>
2024-11-16 15:48:47 -0500

    Finished

--- Factory Workers Jaden White/Factory Workers Jaden White.cpp
+++ Factory Workers Jaden White/Factory Workers Jaden White.cpp
@@ -6,25 +6,37 @@ using namespace std;
 class Employee
 {
 public:
-    void Employeeinfo(string EmployeeName, string EmployeeNumber, string HireDate)
+    void Employeeinfo(string EmployeeName, string EmployeeNumber, string HireDate, int Shift, double payrate)
     {
         cout << "Name: " << EmployeeName << endl << "Employee Number: " << EmployeeNumber << endl << "Hire Date: " << HireDate;
+        Production product;
+        product.Shiftinfo(Shift, payrate);
     }
 private:
+    class Production
+    {
+    public:
+        void Shiftinfo(int Shift, double payrate)
+        {
+            if (Shift == 1)
+            {
+                cout << endl << "Shift: Day" << payrate << endl;
+            }
 
+            if (Shift == 2)
+            {
+                cout << endl << "Shift: Night" << endl;
+            }
+
+            cout << "Hourly Pay Rate: $" << payrate << endl << endl;
+        }
+    };
 };
 
 int main()
 {
-    string name;
-    string number;
-    string hiredate;
-
-    cin >> name;
-    cin >> number;
-    cin >> hiredate;
-
     Employee employee;
-    employee.Employeeinfo(name, number, hiredate);
+    employee.Employeeinfo("Paul Jones", "12345", "10/28/2024", 1, 20.25);
+    employee.Employeeinfo("Harriet Smith", "54321", "6/15/2024", 2, 25.50);
 }
 
