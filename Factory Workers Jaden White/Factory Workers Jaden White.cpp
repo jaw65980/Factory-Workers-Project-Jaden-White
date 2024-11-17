de706323b2435b6de6ff2816ef2674e68c241474
Jaden White <jaw65980@email.vccs.edu>
2024-11-16 23:01:14 -0500

    Finished program

--- Factory Workers Jaden White/Factory Workers Jaden White.cpp
+++ Factory Workers Jaden White/Factory Workers Jaden White.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Employee
 {
 public:
-    void Employeeinfo(int Employeetype, string EmployeeName, string EmployeeNumber, string HireDate, int Shift, double payrate, double AnnualSalary, double AnnualBonus)
+    void Employeeinfo(int Employeetype, string EmployeeName, string EmployeeNumber, string HireDate, int Shift, double payrate, double AnnualSalary, double AnnualBonus, double MonthlyBonus, int RequiredTraining, int AttendedTraining)
     {
         cout << "Name: " << EmployeeName << endl << "Employee Number: " << EmployeeNumber << endl << "Hire Date: " << HireDate;
 
@@ -14,12 +14,22 @@ public:
         {
             Production product;
             product.Shiftinfo(Shift, payrate);
+            cout << endl;
         }
 
         if (Employeetype == 2)
         {
             ShiftSupervisor supervisor;
             supervisor.Supervisorinfo(AnnualSalary, AnnualBonus);
+        }
+
+        if (Employeetype == 3)
+        {
+            Production product;
+            product.Shiftinfo(Shift, payrate);
+
+            TeamLeader teamleader;
+            teamleader.TeamLeaderinfo(MonthlyBonus, RequiredTraining, AttendedTraining);
         }
     }
 private:
@@ -30,7 +40,7 @@ private:
         {
             if (Shift == 1)
             {
-                cout << endl << "Shift: Day" << payrate << endl;
+                cout << endl << "Shift: Day" << endl;
             }
 
             if (Shift == 2)
@@ -38,7 +48,7 @@ private:
                 cout << endl << "Shift: Night" << endl;
             }
 
-            cout << "Hourly Pay Rate: $" << payrate << endl << endl;
+            cout << "Hourly Pay Rate: $" << payrate << endl;
         }
     };
 
@@ -51,13 +61,25 @@ private:
             cout << "Anuual Bonus: $" << AnnualBonus << endl << endl;
         }
     };
+
+    class TeamLeader
+    {
+    public:
+        void TeamLeaderinfo(double MonthlyBonus, int RequiredTraining, int AttendingTraining)
+        {
+            cout << "Montly Bonus: $" << MonthlyBonus << endl;
+            cout << "Require Training: " << MonthlyBonus << endl;
+            cout << "Attending Training: " << MonthlyBonus << endl;
+        }
+    };
 };
 
 int main()
 {
     Employee employee;
-    employee.Employeeinfo(1, "Paul Jones", "12345", "10/28/2024", 1, 20.25, 0, 0);
-    employee.Employeeinfo(1, "Harriet Smith", "54321", "6/15/2024", 2, 25.50, 0, 0);
-    employee.Employeeinfo(2, "Elmer Velasquez", "100325", "1/17/2024", 0, 0, 70000.00, 1000.00);
+    employee.Employeeinfo(1, "Paul Jones", "12345", "10/28/2024", 1, 20.25, 0, 0, 0, 0, 0);
+    employee.Employeeinfo(1, "Harriet Smith", "54321", "6/15/2024", 2, 25.50, 0, 0, 0, 0, 0);
+    employee.Employeeinfo(2, "Elmer Velasquez", "100325", "1/17/2024", 0, 0, 70000.00, 1000.00, 0, 0, 0);
+    employee.Employeeinfo(3, "Mike Rogers", "64327", "2/7/2024", 2, 25.50, 0, 0, 150.00, 20, 10);
 }
 
